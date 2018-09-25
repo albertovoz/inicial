460c7313e3444c17c1a3404a77078a7484d1ee66
alberto <albertocsastre@gmail.com>
2018-09-25 12:34:17 +0200

    cambios de main

---
+++ main.php
@@ -0,0 +1,19 @@
+<?php
+
+$animales = [
+    "Muffinhead" => 14,
+    "Peter" => 4,
+    "Monnie" => 7,
+    "Banh" => 10
+];
+echo $animales["Muffinhead"] . PHP_EOL; // Devuelve 14
+echo $animales["Monnie"];
+
+
+function suma (var1,var2,var3)
+{
+    echo "suma de ejemplo";
+    return suma;
+}
+
+?>
