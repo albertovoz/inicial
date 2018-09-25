91a83403debc154f5905344acf9e776856ace8a7
alberto <albertocsastre@gmail.com>
2018-09-25 13:33:40 +0200

    cambios

--- main.php
+++ main.php
@@ -1,19 +1,19 @@
 <?php
 
-$animales = [
-    "Muffinhead" => 14,
-    "Peter" => 4,
-    "Monnie" => 7,
-    "Banh" => 10
-];
-echo $animales["Muffinhead"] ;
-echo $animales["Monnie"];
+$heroe= array (
+    array("nombre"=>"Hulk"),
+    array("nombre"=>"Spiderman"),
+    array("nombre"=>"Batman"),
+    array("nombre"=>"Superman")
+);
 
 
-function suma (var1,var2,var3)
+function suma ($var1,$var2,$var3)
 {
     echo "suma de ejemplo";
     return suma;
 }
+
+var_dump($heroe);
 
 ?>
